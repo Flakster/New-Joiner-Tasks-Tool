ef0927f56afd16ae4af4139a65999f47e2896caf
Carlos Jose Santamaria <Carlos-Jose.Santamaria@endava.com>
2022-01-18 16:47:13 -0500

    Remove unnecessary logs to console

--- src/app.js
+++ src/app.js
@@ -2,35 +2,28 @@ const express = require('express');
 const app = express();
 const config = require('./utils/config');
 const path = require('path');
-const formidable = require('formidable');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({storage});
 
 // Settings
 app.set('port', config.PORT)
 
 // Routes
-// app.get('/',(req,res) =>{
-//     res.writeHead(200,{'Content-Type':'text/html'})
-//     res.write('<h1>New joiners profiles tool</h1>')
-//     res.write('<h2>Upload form</h2>')
-// })
 
-app.get('/', (req, res) =>{
+app.get('/upload', (req, res) =>{
     res.sendFile(path.join(__dirname , '/views/upload.html'))
 })
 
-app.post('/', (req, res) =>{
-    const form = new formidable.IncomingForm()
-    form.parse(req)
-    
-    form.on('fileBegin', function (name, file) {
-        //console.log('file: ',file.file.name)
-        file.path = path.join(__dirname,  '../uploads/') + file.name
-    })
-    form.on('file', function (name, file) {
-        console.log(" Uploaded file: ", file.name)
-    })
-    res.sendFile(path.join(__dirname + '/views/upload.html'))
-    
+app.post('/', upload.single('uploadFile'), (req, res) =>{
+    res.send('File successfully uploaded')
 })
 
 module.exports = app;
