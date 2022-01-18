2e29eb79a6d4ed9506a5d8e6e57f852ca897b38b
Carlos Jose Santamaria <Carlos-Jose.Santamaria@endava.com>
2022-01-18 11:16:36 -0500

    add routes section to app.js file

--- src/app.js
+++ src/app.js
@@ -5,5 +5,10 @@ const config = require('./utils/config');
 // Settings
 app.set('port', config.PORT)
 
-
+// Routes
+app.get('/',(req,res) =>{
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write('<h1>New Joiners profiles manager</h1>')
+    res.write('<h2>Upload form</h2>')
+})
 module.exports = app;
