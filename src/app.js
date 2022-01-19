8f7c2b24d182b1d92a9fa733a25e9e2d28d8dbb3
Carlos Jose Santamaria <Carlos-Jose.Santamaria@endava.com>
2022-01-19 10:23:41 -0500

    write code for Multer upload to work

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const config = require('./utils/config');
 const path = require('path');
 const multer = require('multer');
+const res = require('express/lib/response');
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './uploads')
@@ -11,10 +12,22 @@ const storage = multer.diskStorage({
         cb(null, file.originalname)
     }
 })
-const upload = multer({storage});
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        if(file.mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || file.mimetype =='application/pdf'){
+            cb(null, true)
+        }
+        else {
+            cb(new Error('Only .docx and pdf format allowed'))
+        }
+    }
+});
+
 
 // Settings
 app.set('port', config.PORT)
+app.use(express.static('public'))
 
 // Routes
 
@@ -22,8 +35,14 @@ app.get('/upload', (req, res) =>{
     res.sendFile(path.join(__dirname , '/views/upload.html'))
 })
 
-app.post('/', upload.single('uploadFile'), (req, res) =>{
-    res.send('File successfully uploaded')
+app.post('/', (req, res) => {
+    upload.single('uploadFile')(req, res, err => {
+        if (err){
+            res.sendFile(path.join(__dirname, '/views/error.html'))
+        }
+        res.sendFile(path.join(__dirname , '/views/success.html'))
+    })
 })
+
 
 module.exports = app;
