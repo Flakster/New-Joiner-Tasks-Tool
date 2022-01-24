ad6bf78902264d3f575975a8e4147835d4d9368e
Carlos Jose Santamaria <Carlos-Jose.Santamaria@endava.com>
2022-01-24 09:57:05 -0500

    refactor string extraction code

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,9 +1,29 @@
+const limitStr = 'Education and Training'
+
+const string_set = str => { 
+    return str.replace(/[\n\r]/g,' ').trim() + '\n'
+}
+
+const string_cut1 = (originalStr, lastString) => {
+    return originalStr.substring(0, originalStr.indexOf(lastString))
+}
+
+const string_cut2 = (originalStr, firstStr, lastStr) => {
+    return originalStr.substring(originalStr.indexOf(firstStr), originalStr.indexOf(lastStr))
+}
+
+const string_cut3 = (originalStr, firstStr) => {
+    return originalStr.substring(originalStr.indexOf(firstStr))
+}
+
 module.exports = {
     extract_info: (text) => {
-        const text1 = text.substring(text.indexOf('Summary:'),text.indexOf('Domain Experience'))
-        const extract = text1.substring(0,text1.indexOf('Role')).replace(/[\n\r]/g,' ').trim() + '\n' +
-            text1.substring(text1.indexOf('Role'),text1.indexOf('Summary')).replace(/[\n\r]/g,' ').trim() + '\n' +
-            text1.substring(text1.indexOf('Summary')).replace(/[\n\r]/g,' ').trim()
+        const text1 = text.substring(0,text.indexOf(limitStr))
+        const extract = string_set(string_cut1(text1,'Role')) +
+        string_set(string_cut2(text1, 'Role', 'Summary')) +
+        string_set(string_cut2(text1, 'Summary', 'Domain Experience')) +
+        string_set(string_cut2(text1, 'Domain Experience', 'Core Technical Skills')) +
+        string_set(string_cut3(text1, 'Core Technical Skills'))
         return extract
     }
 }
