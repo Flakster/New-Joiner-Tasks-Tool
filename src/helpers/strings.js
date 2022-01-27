6c71874ab624d9d55652f6243b0ec3792b4c30b3
Carlos Jose Santamaria <Carlos-Jose.Santamaria@endava.com>
2022-01-27 18:27:51 -0500

    add code to format the extracted info as JSON

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,29 +1,47 @@
 const limitStr = 'Education and Training'
 
+const labels = {
+    "Nam" : 6,
+    "Rol" : 6,
+    "Sum" : 8,
+    "Dom" : 18,
+}
+
+// Remove carriage return and repeated spaces 
 const string_set = str => { 
-    return str.replace(/[\n\r]/g,' ').trim() + '\n'
+    const starting = str.substring(0,3)
+    const text = str.substring(labels[starting])
+    return text.replace(/[\n\r]/g,' ').trim()
+}
+// Organize skills in an object
+const object_set = str =>{
+    let arr = str.split('\n\n').filter(e => e!=='').splice(4)
+    arr.splice(1,0,"Databases")
+    arr.splice(0,0,"Languages & Technologies")
+    const arr2 = [[arr[0], arr[1]], [arr[2], arr[3]], [arr[4], arr[5]], [arr[6], arr[7]]] 
+    const obj = Object.fromEntries(arr2)
+    return obj
 }
 
-const string_cut1 = (originalStr, lastString) => {
-    return originalStr.substring(0, originalStr.indexOf(lastString))
-}
 
-const string_cut2 = (originalStr, firstStr, lastStr) => {
+const string_cut1 = (originalStr, firstStr, lastStr) => {
     return originalStr.substring(originalStr.indexOf(firstStr), originalStr.indexOf(lastStr))
 }
 
-const string_cut3 = (originalStr, firstStr) => {
+const string_cut2 = (originalStr, firstStr) => {
     return originalStr.substring(originalStr.indexOf(firstStr))
 }
 
 module.exports = {
     extract_info: (text) => {
         const text1 = text.substring(0,text.indexOf(limitStr))
-        const extract = string_set(string_cut1(text1,'Role')) +
-        string_set(string_cut2(text1, 'Role', 'Summary')) +
-        string_set(string_cut2(text1, 'Summary', 'Domain Experience')) +
-        string_set(string_cut2(text1, 'Domain Experience', 'Core Technical Skills')) +
-        string_set(string_cut3(text1, 'Core Technical Skills'))
-        return extract
+        const extract = {
+            "Name": string_set(string_cut1(text1,'Name', 'Role')),
+            "Role": string_set(string_cut1(text1, 'Role', 'Summary')),
+            "Summary": string_set(string_cut1(text1, 'Summary', 'Domain Experience')),
+            "Domain Experience": string_set(string_cut1(text1, 'Domain Experience', 'Core Technical Skills')),
+            "Core Technical Skills": object_set(string_cut2(text1, 'Core Technical Skills'))
+        }
+        return JSON.stringify(extract)
     }
 }
