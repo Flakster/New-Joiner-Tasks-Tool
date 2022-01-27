51e572ea2c010c70b0f5cfc16482c38093addf63
Carlos Jose Santamaria <Carlos-Jose.Santamaria@endava.com>
2022-01-27 18:26:49 -0500

    add receive method to the message broker management

--- src/helpers/message_broker.js
+++ src/helpers/message_broker.js
@@ -17,5 +17,26 @@ module.exports = {
                 console.log(" [x] Message sent: %s ", msg);
             });
         });
+    },
+    receive: (msg) =>{
+        amqp.connect(`amqps://${conf.MB_USER}:${conf.MB_PASS}@${conf.MB_URL}`, (error0, connection) => {
+            if (error0) {
+                throw error0;
+            }
+            connection.createChannel(function(error1, channel) {
+                if (error1) {
+                    throw error1;
+                }
+                var queue = 'Profiles';
+                channel.assertQueue(queue, {durable: false});
+                console.log('[x] Waiting  for messages in %s', queue)
+                channel.consume(queue, function(msg) {
+                    console.log('[x] Received %s', msg.content.toString())
+                },
+                {
+                    noAck: true
+                });
+            });
+        });
     }
 }
